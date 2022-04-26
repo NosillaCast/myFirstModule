1c37028dde1992e97cd899193e3f5368138d2fb7
NosillaCast <allison@podfeet.com>
2022-04-25 17:23:44 -0700

    script is entry point using timeCalc module successfully

---
+++ script.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import calcTime from "./timeCalc.mjs"
+
+let values = [3, 47,28]
+calcTime(values);
---
+++ timeCalc.mjs
@@ -0,0 +1,7 @@
+/**
+ * @input {Array} values - the current number of rows of input boxes
+ * 
+ */
+export default function calcTime(values){
+  console.log(`calcTime called with ${values}`);
+}
